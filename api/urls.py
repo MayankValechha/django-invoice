d416f0ee4c898781ac8b9ee3858c9702a0f97b35
MayankValechha <valechhamayank12@gmail.com>
2020-09-06 00:25:27 +0530

    Add new view functions

--- api/urls.py
+++ api/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.api_overview, name='overview'),   
     path('invoices/', views.invoice_list, name='invoice_list'), 
+    path('today/', views.get_latest_entries, name='latest_entry'), 
+    path('invoice/<str:date>/', views.get_by_date),
 ]
